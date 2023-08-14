bb4af0489b7c4b8fa01c1b158fb7101c3babcbc4
Prince <137284876+imparasparabiya@users.noreply.github.com>
2023-08-14 22:28:20 +0530

    Add files via upload

---
+++ Defualt_Constructor.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+//Default Constructor
+class Default{
+	public :
+		Default(){
+			cout<<"***Defult Constructor***"<<endl;
+		}
+	
+};
+
+int main(){
+	
+	Default d;
+	
+	return 0;
+}
+
